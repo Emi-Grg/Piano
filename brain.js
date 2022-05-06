f59d916350db13f9e8fad25d91f9826f9e1ea04d
Ashwot-Acharya <ashwot1133@gmail.com>
2022-05-06 22:55:04 +0545

    Basic Layout of the Project and basic function built

---
+++ brain.js
@@ -0,0 +1,13 @@
+var msg = document.getElementsByClassName('set').innerHtml()
+
+msg.addEventListener("keydown", (event) => {
+    // handle keydown
+});
+
+msg.addEventListener("keypress", (event) => {
+alert(event.key) 
+
+
+});
+
+
---
+++ script.js
@@ -0,0 +1,59 @@
+function playtha(key){
+
+switch (key){
+      case "b":
+          var audio = new Audio('not/b3.mp3');
+          audio.play();
+    break;
+    case "as":
+        var audio = new Audio('not/a-4.mp3');
+        audio.play();
+break;
+  case "a":
+    var audio = new Audio('not/a4.mp3');
+    audio.play();
+break;
+case "gs":
+    var audio = new Audio('not/g-3.mp3');
+    audio.play();
+break;
+case "g":
+    var audio = new Audio('not/g3.mp3');
+    audio.play();
+break;
+case "fs":
+    var audio = new Audio('not/f-3.mp3');
+    audio.play();
+break;
+case "f":
+    var audio = new Audio('not/f3.mp3');
+    audio.play();
+break;
+case "e":
+    var audio = new Audio('not/e3.mp3');
+    audio.play();
+break;
+case "ds":
+    var audio = new Audio('not/d-3.mp3');
+    audio.play();
+break;
+case "d":
+    var audio = new Audio('not/d3.mp3');
+    audio.play();
+break;
+case "cs":
+    var audio = new Audio('not/c-3.mp3');
+    audio.play();
+break;
+case "c":
+    var audio = new Audio('not/c3.mp3');
+    audio.play();
+break;
+ default:
+     alert("ERROR")
+
+
+
+
+  }
+}
